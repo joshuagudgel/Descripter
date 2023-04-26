88162c705647789b4202753491058b2d5e483482
Josh <joshuabgudgel@gmail.com>
2023-04-25 18:50:26 -0700

    Cleanup fresh start

--- main.py
+++ main.py
@@ -1,43 +1,67 @@
 import movie_script as m
 
-path = r"C:\src\Descripter\movie_scripts\knivesout-script.pdf"
+from lib2to3.pgen2.token import NEWLINE
+import pdfplumber
+
+# Helpers
+
+### pdfplumber checks
+def plumberPageToText(path, pageNo):
+    with pdfplumber.open(path) as pdf:
+        page = pdf.pages[pageNo]
+        pageText = ""
+        currLine = page.chars[0]['y0']
+        for c in page.chars:
+            if c['y0'] != currLine:
+                print(c['y0'])
+                pageText+='\n'
+            currLine = c['y0']
+            pageText+=c['text']
+        return pageText
+
+def writeTextToFile(filename,pageText):
+    f = open(filename + ".txt","w+")
+    f.write(pageText)
+    f.close()
+
+# main
+knivesOutPath = r"C:\src\Descripter\movie_scripts\knivesout-script.pdf"
+dunePath = r"C:\src\Descripter\movie_scripts\dune-script.pdf"
+spiderVersePath = r"C:\src\Descripter\movie_scripts\spiderverse-script.pdf"
+"""
 script = m.MovieScript(path)
 pdf = script.getPdf()
+"""
+
+#knivesOutPageText = plumberPageToText(knivesOutPath,2)
+#writeTextToFile("knivesout",knivesOutPageText)
+
+#spiderVersePageText = plumberPageToText(spiderVersePath,2)
+#writeTextToFile("spiderverse",spiderVersePageText)
+
+dunePageText = plumberPageToText(dunePath,4)
+writeTextToFile("dune",dunePageText)
+
 
 ### display cast
-#print(script.getCast())
+"""
+cast = script.getCast()
+cast.sort()
+print(cast)
+"""
 
 # display text from page
-
-#pageText = ""
-#pageNumber = 1
-#for c in pdf[pageNumber].chars:
-#    pageText += c['text']
-#print(pageText[220])
+"""
+pageText = ""
+pageNumber = 2
+for c in pdf[pageNumber].chars:
+    pageText += c['text']
+#print(pageText)
+"""
 
 ### display cast and dialogue from one character
-#for castMember in knivesOutScript.cast:
-#    if castMember == 'WALT':
-#        print(knivesOutScript.dialogue[castMember])
-
-### write to txt file
-#f = open("scriptText.txt","w+")
-#f.write(pageText)
-#f.close()
-
-# find expected values to test movie_script.nextLine()
-manorPos = script.findFirstWord(1,"MANOR")
-manorNextLinePos = script.findFirstWord(1, "The")
-unlitPos = script.findFirstWord(1, "Unlit")
-unlitNextLinePos = script.findFirstWord(1, "arcane")
-franPos = script.findFirstWord(1, "FRAN")
-franNextLinePos = script.findFirstWord(1, "up")
-deadPos = script.findFirstWord(1, "dead.")
-
-print(manorPos)
-print(manorNextLinePos)
-print(unlitPos)
-print(unlitNextLinePos)
-print(franPos)
-print(franNextLinePos)
-print(deadPos)
+"""
+for castMember in knivesOutScript.cast:
+    if castMember == 'WALT':
+        print(knivesOutScript.dialogue[castMember])
+"""
