416b3fdc91cdf66065b7595dd58120391bfb93c9
joshuagudgel <joshuabgudgel@gmail.com>
2023-05-24 20:13:46 -0700

    test cases for first few pages of Knives Out and Dune

--- test_movie_script.py
+++ test_movie_script.py
@@ -1,14 +1,45 @@
-import unittest
-import movie_script as m
+import pytest
+from movie_script import *
 
-class TestMovieScript(unittest.TestCase):
+# Constants
+knivesPath = r"C:\src\Descripter\movie_scripts\knivesout-script-3.pdf"
+knivesCast = ['FRAN', 'MOM', 'ALICE']
+knivesDialogue = {
+    'FRAN' : ['Morning Mr Thrombey', 'Mr Thrombey you up there? Mr Thrombey I\'m coming in', 'Shit.'],
+    'MOM' : ['Now please just turn it off. Turn it off. Now. Alice. Off. They\'re talking about murder on it, your sister just had a friend she loves slit his throat open she doesn\'t need to be hearing that right now let\'s be sensitive!'],
+    'ALICE' : ['Alice, turn that off now.', ]
+}
 
-    def test_nextLine(self):
-        raise NotImplementedError()
+dunePath = r"C:\src\Descripter\movie_scripts\dune-script-3.pdf"
+duneCast = ['FRAN', 'MOM', 'ALICE']
+duneDialogue = {
+    'FRAN' : ['Morning Mr Thrombey', 'Mr Thrombey you up there? Mr Thrombey I\'m coming in', 'Shit.'],
+    'MOM' : ['Now please just turn it off. Turn it off. Now. Alice. Off. They\'re talking about murder on it, your sister just had a friend she loves slit his throat open she doesn\'t need to be hearing that right now let\'s be sensitive!'],
+    'ALICE' : ['Alice, turn that off now.', ]
+}
 
+# Knives Out tests
+# The first 3 pages of pdf
+@pytest.fixture
+def knivesscript():
+    return MovieScript(knivesPath)
 
-    def test_generateCast(self):
-        raise NotImplementedError()
+# Test cases
+def test_knives_cast():
+    assert set(knivesscript.cast) is set(knivesCast)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_knives_dialogue():
+    assert set(knivesscript.dialogue) is set(knivesDialogue)
+
+# Dune tests
+# The first 3 pages of pdf
+@pytest.fixture
+def dunescript():
+    return MovieScript(dunePath)
+
+# Test cases
+def test_dune_cast():
+    assert set(dunescript.cast) is set(duneCast)
+
+def test_dune_dialogue():
+    assert set(dunescript.dialogue) is set(duneDialogue)
